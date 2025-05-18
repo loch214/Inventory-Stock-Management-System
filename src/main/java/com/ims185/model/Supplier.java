ba320c53cb1d68c1b4bde618920137179f1d3c9f
Kaveesha <it24100131@my.sliit.lk>
2025-05-18 21:19:11 +0530

    Created the attributes with getters and setters.

--- src/main/java/com/ims185/model/Supplier.java
+++ src/main/java/com/ims185/model/Supplier.java
@@ -18,5 +18,43 @@ public class Supplier extends Entity {
         this.address = address;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
